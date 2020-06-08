cb8eff8c933737e88a66ec8a2cc1b1f7497d08ae
Daniel Pereira <danielpereira227@gmail.com>
2020-06-08 14:27:08 -0300

    variaveis kotlin

---
+++ src/ma.kt
@@ -0,0 +1,26 @@
+fun main()
+{
+    print("variaveis sao  alocadas dinamicamente ");
+
+    //variaveis dinamicas
+    println();
+    var nome="Daniel";
+    var numero=1;
+    var valor=7.1;
+
+    println(nome)
+    println(numero)
+    println(valor)
+    println()
+
+
+//      variaveis imutaveis
+    println(" variavel pre alocada ");
+
+    var salario:Float= 1.44F;
+
+    //variavel que nao muda o valor
+    val imutavel =10;
+
+
+}
